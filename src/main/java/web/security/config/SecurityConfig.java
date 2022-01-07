e403f7bee54e151a2e41fb765fd9bc4eb1f3ff39
Alex Zherdev <ahriman.kun@gmail.com>
2022-01-07 18:51:00 +0300

    1

--- src/main/java/web/security/config/SecurityConfig.java
+++ src/main/java/web/security/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(12);
+        return new BCryptPasswordEncoder();
     }
 
 
